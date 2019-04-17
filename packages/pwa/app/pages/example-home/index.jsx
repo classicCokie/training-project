c623595e69563aec5dc430fe8d11baf8d814aa35
Richard Sexton <rsexton404@gmail.com>
2019-04-16 22:39:47 -0700

    Add progress bar component

--- packages/pwa/app/pages/example-home/index.jsx
+++ packages/pwa/app/pages/example-home/index.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types'
 import Link from 'progressive-web-sdk/dist/components/link'
 import ListTile from 'progressive-web-sdk/dist/components/list-tile'
 import Banner from 'progressive-web-sdk/dist/components/banner'
+import MyProgressBar from '../../components/my-progress-bar'
 
 import * as actions from './actions'
 import * as globalSelectors from '../../selectors'
@@ -25,6 +26,7 @@ class ExampleHome extends React.Component {
     render() {
         return (
             <div className="t-home">
+                <MyProgressBar percentageComplete={30} hasLabel={true} />
                 <h1 className="u-padding-top-md u-margin-bottom-sm">Homepage</h1>
                     <Banner icon="info" title="info">
                         Free Shipping on orders over $50
